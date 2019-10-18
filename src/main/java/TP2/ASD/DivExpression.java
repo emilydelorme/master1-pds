56e4cb60e0e15f7f90bbe066b61ec47904565e53
Brandon Largeau <yberion.ins@gmail.com>
2019-10-18 16:01:13 +0200

    [ASD] DivExpression

---
+++ src/main/java/TP2/ASD/DivExpression.java
@@ -0,0 +1,49 @@
+package TP2.ASD;
+
+import TP2.Llvm;
+import TP2.TypeException;
+import TP2.Utils;
+
+// Concrete class for Expression: div case
+  public class DivExpression extends Expression {
+    Expression left;
+    Expression right;
+
+    public DivExpression(Expression left, Expression right) {
+      this.left = left;
+      this.right = right;
+    }
+
+    // Pretty-printer
+    public String pp() {
+      return "(" + left.pp() + " / " + right.pp() + ")";
+    }
+
+    // IR generation (IR = Représentation intermédiaire)
+    public RetExpression toIR() throws TypeException {
+      RetExpression leftRet = left.toIR();
+      RetExpression rightRet = right.toIR();
+
+      // We check if the types mismatches
+      if(!leftRet.type.equals(rightRet.type)) {
+        throw new TypeException("type mismatch: have " + leftRet.type + " and " + rightRet.type);
+      }
+
+      // We base our build on the left generated IR:
+      // append right code
+      leftRet.ir.append(rightRet.ir);
+
+      // allocate a new identifier for the result
+      String result = Utils.newtmp();
+
+      // new div instruction result = left * right
+      Llvm.Instruction div = new Llvm.Div(leftRet.type.toLlvmType(), leftRet.result, rightRet.result, result);
+
+      // append this instruction
+      leftRet.ir.appendCode(div);
+
+      // return the generated IR, plus the type of this expression
+      // and where to find its result
+      return new RetExpression(leftRet.ir, leftRet.type, result);
+    }
+  }
