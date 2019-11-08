49f5f9abd610be4fdd727e9ee04c17663f51cea4
Emily Delorme <emilydelormefr@gmail.com>
2019-11-08 14:48:23 +0100

    Refractoring Type, converting abstract class to interface

--- src/main/java/TP2/ASD/Type.java
+++ src/main/java/TP2/ASD/Type.java
@@ -3,7 +3,7 @@ package TP2.ASD;
 import TP2.Llvm;
 
 // Warning: this is the type from VSL+, not the LLVM types!
-  public abstract class Type {
-    public abstract String pp();
-    public abstract Llvm.Type toLlvmType();
+  public interface Type {
+    String pp();
+    Llvm.Type toLlvmType();
   }
