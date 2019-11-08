17b2cae1bd8f9d5e84f19f6385cede5f38efbdde
Emily Delorme <emilydelormefr@gmail.com>
2019-11-08 15:10:32 +0100

    Creating base class for Block

---
+++ src/main/java/TP2/ASD/Statement/Block.java
@@ -0,0 +1,29 @@
+package TP2.ASD.Statement;
+
+import TP2.ASD.StatementInterface;
+import TP2.exceptions.TypeException;
+
+import java.util.List;
+
+public class Block implements StatementInterface {
+
+    private List<StatementInterface> statements;
+
+    public Block(List<StatementInterface> statements) {
+        this.statements = statements;
+    }
+
+    @Override
+    public String pp() {
+        return "{"+
+                statements.stream()
+                        .map(StatementInterface::pp)
+                        .reduce((statement1, statement2) -> statement1 + "\n" + statement2)
+                +"}";
+    }
+
+    @Override
+    public RetInstruction toIR() throws TypeException {
+        return null;
+    }
+}
