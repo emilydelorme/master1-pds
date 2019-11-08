2230a6ab0399186dbefcf21a48e2bb37344cea99
Emily Delorme <emilydelormefr@gmail.com>
2019-11-08 11:19:35 +0100

    Add declaration class

--- src/main/java/TP2/ASD/Declaration.java
+++ src/main/java/TP2/ASD/Declaration.java
@@ -2,5 +2,15 @@ package TP2.ASD;
 
 public class Declaration
 {
+    private String ident;
+
+    public Declaration(String ident) {
+        this.ident = ident;
+    }
+    // Pretty-printer
+    public String pp()
+    {
+        return this.ident;
+    }
     
 }
