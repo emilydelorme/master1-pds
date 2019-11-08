1e1c4727a95c55e192f0ff98c94c3bb95104a7ae
Emily Delorme <emilydelormefr@gmail.com>
2019-11-08 14:48:36 +0100

    Add void type in ASD

---
+++ src/main/java/TP2/ASD/types/Void.java
@@ -0,0 +1,17 @@
+package TP2.ASD.types;
+
+import TP2.ASD.Type;
+import TP2.Llvm;
+
+public class Void implements Type {
+
+    @Override
+    public String pp() {
+        return "VOID";
+    }
+
+    @Override
+    public Llvm.Type toLlvmType() {
+        return Llvm.Void();
+    }
+}
