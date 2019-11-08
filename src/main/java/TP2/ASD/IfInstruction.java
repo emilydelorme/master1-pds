229dc89ce15b3151ce2d5f236e0b7c50a26ad664
Emily Delorme <emilydelormefr@gmail.com>
2019-11-08 12:48:14 +0100

    Add if instruction
    TODO: finish toIR() function

---
+++ src/main/java/TP2/ASD/IfInstruction.java
@@ -0,0 +1,29 @@
+package TP2.ASD;
+
+import TP2.exceptions.TypeException;
+
+import java.util.List;
+
+public class IfInstruction implements Instruction {
+
+    private Expression expression;
+    private List<Instruction> block;
+
+    public IfInstruction(Expression expression, List<Instruction> block) {
+        this.expression = expression;
+        this.block = block;
+    }
+
+    @Override
+    public String pp() {
+        return "if(" + expression.pp() + ")\n" + block.stream()
+                .map(Instruction::pp)
+                .reduce((statement, statement2) -> statement + "\n" + statement2);
+    }
+
+    @Override
+    public RetIntruction toIR() throws TypeException {
+        //TODO: IF to IR()
+        return null;
+    }
+}
