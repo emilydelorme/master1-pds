ea04232ba49e122f9bfe803211dfe3d66cac65a6
Brandon <Brandon.Largeau@gmail.com>
2019-11-13 14:48:34 +0100

    Format StatementInterface

--- src/main/java/TP2/ASD/StatementInterface.java
+++ src/main/java/TP2/ASD/StatementInterface.java
@@ -2,7 +2,8 @@ package TP2.ASD;
 
 import TP2.exceptions.TypeException;
 
-public interface StatementInterface {
+public interface StatementInterface
+{
     String pp();
     Ret toIR() throws TypeException;
 }
