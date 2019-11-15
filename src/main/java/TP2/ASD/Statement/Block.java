3a1532fa8526935a2347bc57d43e7be277235465
Brandon Largeau <yberion.ins@gmail.com>
2019-11-15 15:20:23 +0100

    Update Block

--- src/main/java/TP2/ASD/Statement/Block.java
+++ src/main/java/TP2/ASD/Statement/Block.java
@@ -1,48 +1,55 @@
 package TP2.ASD.Statement;
 
+import java.util.List;
+
 import TP2.ASD.Ret;
 import TP2.ASD.StatementInterface;
+import TP2.ASD.Declaration.Declaration;
 import TP2.exceptions.TypeException;
-
-import java.util.List;
 
 public class Block implements StatementInterface
 {
+    private Declaration declaration;
     private List<StatementInterface> statements;
 
-    public Block(List<StatementInterface> statements)
+    public Block(Declaration declaration, List<StatementInterface> statements)
     {
+        this.declaration = declaration;
         this.statements = statements;
     }
 
     @Override
-    public String pp() {
+    public String pp()
+    {
         String str = "";
-        
+
         int statementSize = this.statements.size();
-        
+
         str += "{";
         str += "\n";
-        
+
+        str += this.declaration.pp();
+
         for (int i = 0; i < statementSize; ++i)
         {
             str += "\t";
             str += this.statements.get(i).pp();
-            
+
             if (i < statementSize - 1)
             {
                 str += "\n";
             }
         }
-        
+
         str += "\n";
         str += "}";
-        
+
         return str;
     }
 
     @Override
-    public Ret toIR() throws TypeException {
+    public Ret toIR() throws TypeException
+    {
 
         return null;
     }
