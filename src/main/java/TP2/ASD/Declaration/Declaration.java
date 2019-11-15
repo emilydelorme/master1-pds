55afd8b069e47949168ef3394656fabcfeb21a36
Brandon Largeau <yberion.ins@gmail.com>
2019-11-15 15:20:12 +0100

    Modification of Declaration, simple use atm

---
+++ src/main/java/TP2/ASD/Declaration/Declaration.java
@@ -0,0 +1,49 @@
+package TP2.ASD.Declaration;
+
+import java.util.List;
+
+import TP2.ASD.Ret;
+import TP2.ASD.TypeInterface;
+import TP2.exceptions.TypeException;
+
+public class Declaration
+{
+    private TypeInterface type;
+    private List<String> idents;
+
+    public Declaration(TypeInterface type, List<String> idents)
+    {
+        this.type = type;
+        this.idents = idents;
+    }
+
+    // Pretty-printer
+    public String pp()
+    {
+        String str = "";
+
+        str += "\t";
+        str += this.type.pp() + " ";
+
+        int identsSize = this.idents.size();
+
+        for (int i = 0; i < identsSize; ++i)
+        {
+            str += this.idents.get(i);
+
+            if (i < identsSize - 1)
+            {
+                str += ", ";
+            }
+        }
+
+        str += "\n";
+
+        return str;
+    }
+
+    public Ret toIR() throws TypeException
+    {
+        return null;
+    }
+}
