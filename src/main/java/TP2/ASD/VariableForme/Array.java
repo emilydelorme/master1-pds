9cafe46ef683239a62bd71173ca7201f5eb4bd11
Brandon <Brandon.Largeau@gmail.com>
2019-11-26 03:41:47 +0100

    Allow expression in Array

--- src/main/java/TP2/ASD/VariableForme/Array.java
+++ src/main/java/TP2/ASD/VariableForme/Array.java
@@ -1,25 +1,26 @@
 package TP2.ASD.VariableForme;
 
 import TP2.ASD.VariableFormeInterface;
+import TP2.ASD.ExpressionInterface;
 import TP2.ASD.Ret;
 import TP2.exceptions.TypeException;
 
 public class Array implements VariableFormeInterface
 {
     private String ident;
-    private int nbSpace;
+    private ExpressionInterface expression;
 
-    public Array(String ident, int nbSpace)
+    public Array(String ident, ExpressionInterface expression)
     {
         this.ident = ident;
-        this.nbSpace = nbSpace;
+        this.expression = expression;
     }
 
     // Pretty-printer
     @Override
     public String pp()
     {
-        return this.ident + "[" + this.nbSpace + "]";
+        return this.ident + "[" + this.expression.pp() + "]";
     }
 
     @Override
