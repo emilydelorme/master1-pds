1ee610b84299e691d01c073d95f19a536939c262
Emily Delorme <emilydelormefr@gmail.com>
2019-12-07 19:39:06 +0100

    Add Llvm alloca

--- src/main/java/TP2/Llvm/Instructions/alloca/AllocaVar.java
+++ src/main/java/TP2/Llvm/Instructions/alloca/AllocaVar.java
@@ -1,14 +1,14 @@
-package TP2.Llvm.Instructions;
+package TP2.Llvm.Instructions.alloca;
 
 
 import TP2.Llvm.Instruction;
 import TP2.Llvm.Type;
 
-public class Alloca implements Instruction {
-    private Type type;
-    private String value;
+public class AllocaVar implements Instruction {
+    final private Type type;
+    final private String value;
 
-    public Alloca(Type type, String lvalue)
+    public AllocaVar(Type type, String lvalue)
     {
         this.type = type;
         this.value = lvalue;
