086f3225f5a512f8abde345f70cd21ad0132229c
Emily Delorme <emilydelormefr@gmail.com>
2019-12-08 14:30:39 +0100

    WIP: affection
    Need more info to complete

--- src/main/java/TP2/ASD/Statement/VariableFormAffectation/Basic.java
+++ src/main/java/TP2/ASD/Statement/VariableFormAffectation/Basic.java
@@ -2,17 +2,25 @@ package TP2.ASD.Statement.VariableFormAffectation;
 
 import TP2.ASD.Ret.GenericRet;
 import TP2.ASD.VariableFormDeclarationInterface;
+import TP2.Llvm.Instructions.Store;
+import TP2.Llvm.Types.LlvmInt;
 import TP2.SymbolTable.SymbolTable;
 
 public class Basic implements VariableFormDeclarationInterface
 {
 
+    private final String ident;
+    private final String pointer;
 
+    public Basic(String ident, String pointer) {
+        this.ident = ident;
+        this.pointer = pointer;
+    }
 
     @Override
     public String getIdent()
     {
-        return null;
+        return ident;
     }
 
     @Override
@@ -24,6 +32,10 @@ public class Basic implements VariableFormDeclarationInterface
     @Override
     public GenericRet toIR(SymbolTable symbolTable)
     {
-        return null;
+        GenericRet result = new GenericRet();
+
+        result.getIr().appendCode(new Store(new LlvmInt(), ident, pointer));
+
+        return result;
     }
 }
