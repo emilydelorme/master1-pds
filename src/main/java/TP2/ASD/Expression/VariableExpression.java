57bff7960a9255b744cd0c29efd04cb3fd3ad120
Emily Delorme <emilydelormefr@gmail.com>
2019-12-08 23:08:11 +0100

    Add VariableExpression toIR code

--- src/main/java/TP2/ASD/Expression/VariableExpression.java
+++ src/main/java/TP2/ASD/Expression/VariableExpression.java
@@ -2,51 +2,52 @@ package TP2.ASD.Expression;
 
 import TP2.ASD.ErrorHandlerInterface;
 import TP2.ASD.ExpressionInterface;
+import TP2.ASD.Ret.GenericRet;
 import TP2.ASD.Ret.TypeRet;
+import TP2.ASD.Types.Int;
+import TP2.Llvm.Instructions.load.LoadVar;
 import TP2.SymbolTable.Symbol;
 import TP2.SymbolTable.SymbolTable;
 import TP2.SymbolTable.VariableSymbol;
 import TP2.ASD.VariableFormInterface;
+import TP2.Utils;
 
-public class VariableExpression implements ExpressionInterface, ErrorHandlerInterface
-{
+public class VariableExpression implements ExpressionInterface, ErrorHandlerInterface {
     private VariableFormInterface variableForm;
     private SymbolTable symbolTable;
 
-    public VariableExpression(VariableFormInterface variableForm, SymbolTable symbolTable)
-    {
+    public VariableExpression(VariableFormInterface variableForm, SymbolTable symbolTable) {
         this.variableForm = variableForm;
         this.symbolTable = symbolTable;
     }
 
     @Override
-    public void checkError()
-    {
+    public void checkError() {
         String variableIdent = this.variableForm.getIdent();
         Symbol symbol = this.symbolTable.lookup(variableIdent);
-        
-        if (!(symbol instanceof VariableSymbol))
-        {
+
+        if (!(symbol instanceof VariableSymbol)) {
             exitWithMessage(String.format("[Variable] (%s) unknown variable", variableIdent));
         }
     }
 
-    public VariableFormInterface getVariableForm()
-    {
+    public VariableFormInterface getVariableForm() {
         return variableForm;
     }
 
-    public String pp()
-    {
+    public String pp() {
         checkError();
-        
+
         return this.variableForm.pp();
     }
 
-    public TypeRet toIR()
-    {
+    public TypeRet toIR() {
         checkError();
-        
-        return null;
+
+        TypeRet result = new TypeRet(new Int());
+        String tmpIdent = Utils.newtmp();
+        result.getIr().appendCode(new LoadVar(tmpIdent, variableForm.getIdent()));
+
+        return result;
     }
 }
