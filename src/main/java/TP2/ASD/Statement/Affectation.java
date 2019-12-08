dbf273be3ae1d6eb9d9e03cea387d848617fbcf1
Brandon <Brandon.Largeau@gmail.com>
2019-12-08 18:04:00 +0100

    Fix bad import

--- src/main/java/TP2/ASD/Statement/Affectation.java
+++ src/main/java/TP2/ASD/Statement/Affectation.java
@@ -1,11 +1,10 @@
 package TP2.ASD.Statement;
 
-
 import TP2.ASD.ExpressionInterface;
 import TP2.ASD.Ret.GenericRet;
-import TP2.ASD.Statement.VariableFormAffectation.Array;
 import TP2.ASD.StatementInterface;
 import TP2.ASD.Types.Void;
+import TP2.ASD.VariableForm.Array;
 import TP2.ASD.VariableFormInterface;
 import TP2.Llvm.Instructions.Store;
 import TP2.Llvm.Instructions.load.LoadTab;
@@ -14,48 +13,57 @@ import TP2.SymbolTable.*;
 import TP2.Utils;
 import TP2.exceptions.TypeException;
 
-public class Affectation implements StatementInterface {
+public class Affectation implements StatementInterface
+{
     private VariableFormInterface leftVar;
     private ExpressionInterface expression;
     private SymbolTable symbolTable;
 
-    public Affectation(VariableFormInterface leftVar, ExpressionInterface expression, SymbolTable symbolTable) {
+    public Affectation(VariableFormInterface leftVar, ExpressionInterface expression, SymbolTable symbolTable)
+    {
         this.leftVar = leftVar;
         this.expression = expression;
         this.symbolTable = symbolTable;
     }
 
     @Override
-    public void checkError() {
+    public void checkError()
+    {
         this.leftVar.checkError();
         this.expression.checkError();
 
-        if (!(this.leftVar instanceof Array)) {
+        if (!(this.leftVar instanceof Array))
+        {
             Symbol symbol = this.symbolTable.lookup(this.leftVar.getIdent());
 
-            if (symbol instanceof VariableSymbol) {
+            if (symbol instanceof VariableSymbol)
+            {
                 VariableSymbol variableSymbol = (VariableSymbol) symbol;
 
-                if (variableSymbol.isArray()) {
+                if (variableSymbol.isArray())
+                {
                     exitWithMessage(String.format("[Affectation] (%s) needs to be called as an array",
-                                                  variableSymbol.getIdent()));
+                            variableSymbol.getIdent()));
                 }
             }
         }
 
-        if (this.leftVar instanceof Array) {
+        if (this.leftVar instanceof Array)
+        {
             Symbol symbol = this.symbolTable.lookup(this.leftVar.getIdent());
 
-            if (symbol instanceof VariableSymbol) {
+            if (symbol instanceof VariableSymbol)
+            {
                 VariableSymbol variableSymbol = (VariableSymbol) symbol;
 
-                if (!variableSymbol.isArray()) {
+                if (!variableSymbol.isArray())
+                {
                     exitWithMessage(String.format("[Affectation] (%s) needs to be a normal variable",
-                                                  variableSymbol.getIdent()));
+                            variableSymbol.getIdent()));
                 }
             }
         }
-        
+
         /*
         if (this.variableForme instanceof Basic)
         {
@@ -78,42 +86,48 @@ public class Affectation implements StatementInterface {
             }
         }
         */
-
-        if (this.expression instanceof FunctionCall) {
+        
+        if (this.expression instanceof FunctionCall)
+        {
             FunctionCall functionCall = (FunctionCall) this.expression;
 
             Symbol symbol = this.symbolTable.lookup(functionCall.getFunctionIdent());
 
-            if (symbol instanceof FunctionSymbol) {
+            if (symbol instanceof FunctionSymbol)
+            {
                 FunctionSymbol functionSymbol = (FunctionSymbol) symbol;
 
-                if (functionSymbol.getReturnType().equals(new Void())) {
+                if (functionSymbol.getReturnType().equals(new Void()))
+                {
                     exitWithMessage(String.format("[Affectation] (%s) can't call a [%s] function",
-                                                  functionSymbol.getIdent(), functionSymbol.getReturnType().getType()));
+                            functionSymbol.getIdent(), functionSymbol.getReturnType().getType()));
                 }
             }
 
-            if (symbol instanceof PrototypeSymbol) {
+            if (symbol instanceof PrototypeSymbol)
+            {
                 PrototypeSymbol prototypeSymbol = (PrototypeSymbol) symbol;
 
-                if (prototypeSymbol.getReturnType().equals(new Void())) {
+                if (prototypeSymbol.getReturnType().equals(new Void()))
+                {
                     exitWithMessage(String.format("[Affectation] (%s) can't call a [%s] function",
-                                                  prototypeSymbol.getIdent(),
-                                                  prototypeSymbol.getReturnType().getType()));
+                            prototypeSymbol.getIdent(), prototypeSymbol.getReturnType().getType()));
                 }
             }
         }
     }
 
     @Override
-    public String pp() {
+    public String pp()
+    {
         checkError();
 
         return this.leftVar.pp() + " := " + this.expression.pp();
     }
 
     @Override
-    public GenericRet toIR(SymbolTable symbolTable) throws TypeException {
+    public GenericRet toIR(SymbolTable symbolTable) throws TypeException
+    {
         checkError();
 
         GenericRet result = new GenericRet();
@@ -123,21 +137,27 @@ public class Affectation implements StatementInterface {
 
         String leftVarIdent = "";
 
-        if (leftVar instanceof Array) {
+        if (leftVar instanceof Array)
+        {
             Symbol leftSymbol = symbolTable.lookup(leftVar.getIdent());
             String resultIdent = Utils.newtmp();
             GenericRet leftRet = leftVar.toIR();
 
-            if (leftRet.getIr().isEmpty()) {
+            if (leftRet.getIr().isEmpty())
+            {
                 result.getIr().appendAll(leftRet.getIr());
-                result.getIr().appendCode(new LoadTab(resultIdent,
-                                                      "%" + leftVar.getIdent(),
-                                                      leftRet.getResult(),
-                                                      leftSymbol.getIdent())); //TODO: change with index
+                result.getIr().appendCode(
+                        new LoadTab(resultIdent, 
+                                "%" + leftVar.getIdent(),
+                                leftRet.getResult(),
+                                leftSymbol.getIdent())); // TODO: change with index
             }
+            
             leftVarIdent = resultIdent;
 
-        } else {
+        }
+        else
+        {
             // maybe Symbol leftSymbol = symbolTable.lookup(leftVar.getIdent());
             leftVarIdent = "%" + leftVar.getIdent(); // Add index
         }
