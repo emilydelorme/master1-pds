0de82f1ccb429e8daac0eb2f32a05d44f78232ad
Emily Delorme <emilydelormefr@gmail.com>
2019-12-09 22:33:12 +0100

    Add NoVslFile exception and fix indent in TestUtils

--- src/main/java/TP2/exceptions/NoVslFileException.java
+++ src/main/java/TP2/exceptions/NoVslFileException.java
@@ -203,67 +203,12 @@
  *
  */
 
-package TP2;
+package TP2.exceptions;
 
-import TP2.ASD.Program;
-import TP2.exceptions.EmptyProgram;
-import TP2.exceptions.TypeException;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.tinylog.Logger;
+public class NoVslFileException extends Exception {
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Objects;
 
-public class TestUtils {
-
-    private TestUtils() {}
-
-    public static boolean testFolder(String path) throws EmptyProgram, TypeException, IOException {
-        File dir = new File(path);
-        File[] files = dir.listFiles((dir1, name) -> name.endsWith(".vsl"));
-        Files.createDirectories(Paths.get("build/llvm/test"));
-
-        for (File vslfile : Objects.requireNonNull(files)) {
-            Logger.info("Compiling " + vslfile.getPath());
-            boolean result = testFile(vslfile.getPath());
-            if (!result)
-                return false;
-        }
-        return true;
-    }
-
-    public static boolean testFile(String path) throws IOException, EmptyProgram, TypeException {
-        // Instantiate Lexer
-        VSLLexer lexer = new VSLLexer(CharStreams.fromPath(Paths.get(path)));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        // Instantiate Parser
-        VSLParser parser = new VSLParser(tokens);
-
-        // Parse
-        Program ast = parser.program().out;
-
-        Logger.debug(ast.pp());
-
-        String ir = ast.toIR().toString();
-        Logger.debug(ir);
-        String llvmFilePath = "build/llvm/test/" + Paths.get(path).getFileName().toString().substring(0,
-                                         Paths.get(path).getFileName().toString().length() - 3) + "ll";
-        writeFile(llvmFilePath, ir);
-
-        return true;
-    }
-
-    public static void writeFile(String path, String content) {
-        try (FileOutputStream outputStream = new FileOutputStream(path)) {
-            outputStream.write(content.getBytes());
-        } catch (IOException e) {
-            Logger.error(e.getMessage());
-        }
+    public NoVslFileException(String message) {
+        super(message);
     }
 }
